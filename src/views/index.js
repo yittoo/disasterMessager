3fea969dcb6f2c9ea51ddffa239932428e0a366a
yitt <sozer.work@gmail.com>
2020-02-19 18:04:52 +0300

    remove unused main view

--- src/views/index.js
+++ src/views/index.js
@@ -1,4 +1,3 @@
-export {default as Main} from './Main';
 export {default as Settings} from './Settings';
 export {default as ContactsSelector} from './ContactsSelector';
 export {default as Sms} from './Sms';
